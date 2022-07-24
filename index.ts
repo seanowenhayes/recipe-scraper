3f9bad7b41b4e026994f1a8309bb142f5f066390
= <seanowenhayes@gmail.com>
2022-07-24 08:05:53 +0200

    Add data extraction

--- index.ts
+++ index.ts
@@ -1,13 +1,22 @@
 import { Crawler } from "./lib/crawler.ts";
 
-export interface Extractor {
-  linkExtraction: string;
+interface Extractor {
   shouldExtract: (pageUrl: string) => boolean;
+}
+export interface LinkExtractor extends Extractor {
+  linkExtraction: string;
+}
+
+export interface DetailExtractor extends Extractor {
+  details: {
+    [key: string]: string;
+  };
 }
 export interface Config {
   crawl: {
     startUrl: string;
-    linkExtractors: Extractor[];
+    linkExtractors: LinkExtractor[];
+    detailExtractor: DetailExtractor;
   };
   crawler: {
     launchConfig: {
@@ -22,10 +31,44 @@ const config: Config = {
     linkExtractors: [
       {
         linkExtraction: ".recipe-block a",
-        shouldExtract: (url) =>
-          url.startsWith("https://www.jamieoliver.com/recipes"),
+        shouldExtract: (url) => {
+          const rawUrl = new URL(url);
+          if (rawUrl.pathname === "/recipes/category/ingredient/") {
+            return true;
+          }
+          if (rawUrl.pathname.split("/").length === 4) {
+            return true;
+          }
+          return false;
+        },
       },
     ],
+    detailExtractor: {
+      details: {
+        headline: "h1",
+        subHeadline: "p.subheading",
+        serves: ".recipe-detail.serves",
+        time: ".recipe-detail.time",
+        difficulty: ".recipe-detail.difficulty",
+        calories: "li[title=Calories] .top",
+        fat: "li[title=Fat] .top",
+        saturates: "li[title=Saturates] .top",
+        sugars: "li[title=Sugars] .top",
+        salt: "li[title=Salt] .top",
+        protein: "li[title=Protein] .top",
+        carbohydrate: "li[title=Carbs] .top",
+        fibre: "li[title=Fibre] .top",
+        ingredients: ".ingred-list",
+        method: "div.method-p > div",
+      },
+      shouldExtract: (url) => {
+        const rawUrl = new URL(url);
+        if (rawUrl.pathname.split("/").length === 5) {
+          return true;
+        }
+        return false;
+      },
+    },
   },
   crawler: {
     launchConfig: {
@@ -51,5 +94,6 @@ const config: Config = {
   crawler.on("info", (tag, info) => {
     console.log("info", tag, info);
   });
+  crawler.on("data", (record) => console.log({ record }));
   await crawler.crawl();
 })();
